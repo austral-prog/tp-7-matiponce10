04f473b6214a5df99606cef4812902c62c64186d
Matias Ponce <matias.ponce@mulesoft.com>
2024-05-15 21:07:15 -0300

    Solutions

--- loops.py
+++ loops.py
@@ -1,18 +1,36 @@
 def index_of_by_index(word, list, index):
+    for i, elemento in enumerate(list[index:], index):
+        if elemento == word:
+            return i
     return -1
 
 
 def index_of_empty(list):
+    for i, elemento in enumerate(list):
+        if elemento == "":
+            return i
     return -1
 
 
 def index_of(word, list):
+    for i, elemento in enumerate(list):
+        if elemento == word:
+            return i
     return -1
 
 
 def put(word, list):
+    for i, elemento in enumerate(list):
+        if elemento == "":
+            list[i] = word
+            return i
     return -1
 
 
 def remove(word, list):
-    return -1
+    borrados = 0
+    for i, elemento in enumerate(list):
+        if elemento == word:
+            list[i] = ""
+            borrados += 1
+    return borrados
--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,18 @@
 def enumerate_list(list):
-    return list
+    new_list = []
+    i = 0
+    for color in list:
+        if color != "":
+            new_list.append(f"{i}. {color}")
+            i +=1
+    return new_list
 
 
 def enumerate_backwards(list):
-    return list
+    new_list = []
+    i = 0
+    for color in list:
+        if color != "":
+            new_list.append(f"{i}. {color[::-1]}")
+            i += 1
+    return new_list
